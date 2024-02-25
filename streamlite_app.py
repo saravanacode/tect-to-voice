3d0353383ac79a17977f40a34b5d6988d3cf734d
saravanacode <141404319+saravanacode@users.noreply.github.com>
2024-02-25 14:34:24 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -23,6 +23,7 @@ def clear():
     st.empty()
 
 st.title("ChatBot")
+face_img = st.image("women_wonder.gif", use_column_width=True)
 
 prompt = st.text_input("Enter your question:")
 st.write("")  # Adding some space between input and buttons
