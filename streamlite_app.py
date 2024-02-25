161c0936923b4ea60562ff1c79e045c0748f55f0
saravanacode <141404319+saravanacode@users.noreply.github.com>
2024-02-25 15:38:13 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,28 +1,62 @@
 import streamlit as st
-from openai import OpenAI
+import ollama
+from typing import Dict, Generator
+from gtts import gTTS
+from io import BytesIO
 
-st.title("ChatGPT-like clone")
+def ollama_generator(model_name: str, messages: Dict) -> Generator:
+    stream = ollama.chat(
+        model=model_name, messages=messages, stream=True)
+    for chunk in stream:
+        yield chunk['message']['content']
 
-# Set OpenAI API key from Streamlit secrets
-client = OpenAI(api_key=st.secrets["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"])
 
-# Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+import base64
 
-# Initialize chat history
+
+
+def autoplay_audio(file_path):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+
+
+sound_file = BytesIO()
+st.title("Ollama with Streamlit demo")
+if "selected_model" not in st.session_state:
+    st.session_state.selected_model = ""
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
-# Display chat messages from history on app rerun
+st.session_state.selected_model = st.selectbox(
+    "Please select the model:", [model["name"] for model in ollama.list()["models"]])
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
-# Accept user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("How could I help you?"):
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "user", "content": "restrcit your response within more then 20 lines but it should be a exact answer this is my question :"+ " " + prompt})
     # Display user message in chat message container
     with st.chat_message("user"):
-        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(ollama_generator(
+            st.session_state.selected_model, st.session_state.messages))
+        tts = gTTS(str(response), lang='en')
+        tts.write_to_fp(sound_file)
+        tts.save('sample.wav')
+
+
+        autoplay_audio('sample.wav')
+        # st.audio(sound_file,autoplay=True)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response})
